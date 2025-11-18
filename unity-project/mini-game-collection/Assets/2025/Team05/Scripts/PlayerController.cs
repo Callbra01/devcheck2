0237526d863c7ea8186c71339bab84345b876cdb
Callbra01 <000954560@mohawkcollege.ca>
2025-11-18 00:08:20 -0500

    Add obstacle setup TODO write obstacle manager

--- unity-project/mini-game-collection/Assets/2025/Team05/Scripts/PlayerController.cs
+++ unity-project/mini-game-collection/Assets/2025/Team05/Scripts/PlayerController.cs
@@ -79,7 +79,7 @@ namespace MiniGameCollection.Games2025.Team05
                 canMove = true;
             }
 
-            ScoreKeeper.instance.AddScore(this.PlayerID, 10);
+            ScoreKeeper.instance.AddScore(this.PlayerID, 1);
         }
 
         // Hit timer for flashing player sprite, as well as disabling player collision
@@ -149,7 +149,6 @@ namespace MiniGameCollection.Games2025.Team05
             {
                 Component component = collision.gameObject.GetComponent<TagScorebar>();
                 ScoreKeeper.AddScore(this.PlayerID, 500);
-
 
             }
         }
